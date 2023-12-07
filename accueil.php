9385ba33bade69612139ecb294213242567eac98
DCodeProg <danael.legrand@outlook.fr>
2023-12-07 22:52:17 +0100

    changement accueil

--- accueil.php
+++ accueil.php
@@ -7,37 +7,14 @@ $PAGENAME = "Accueil";
 
 
 <?php require_once(ROOT_DIR. 'includes/partials/header.php');?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>Document</title>
-</head>
 
-<!-- Sticky header -->
-<header class="header-outer">
-	<div class="header-inner responsive-wrapper">
-		<div class="header-logo">
-			<img src="assets/img/logo_site.png" />
-		</div>
-		<nav class="header-navigation">
-			<a href="#">Acceuil</a>
-			<a href="#">A propos</a>
-			<a href="#">Articles</a>
-			<button>Menu</button>
-		</nav>
-	</div>
-</header>
-<!-- Content -->
 <main class="main">
-	<div class="main-content responsive-wrapper">
-		<?php include ROOT_DIR.'widgets/la_terre_cest_quoi.php';?>
-		<?php include ROOT_DIR.'widgets/le_climat_cest_quoi.php';?>
-		<?php include ROOT_DIR.'widgets/GIEC.php';?>
-		<?php include ROOT_DIR.'widgets/HCC.php';?>
-		<?php include ROOT_DIR.'widgets/rechauffement.php';?>
-		<?php include ROOT_DIR.'widgets/temp.php';?>
-	</div>
+    <?php include ROOT_DIR.'widgets/la_terre_cest_quoi.php';?>
+    <?php include ROOT_DIR.'widgets/le_climat_cest_quoi.php';?>
+    <?php include ROOT_DIR.'widgets/GIEC.php';?>
+    <?php include ROOT_DIR.'widgets/HCC.php';?>
+    <?php include ROOT_DIR.'widgets/rechauffement.php';?>
+    <?php include ROOT_DIR.'widgets/temp.php';?>
 </main>
+
+<?php require_once(ROOT_DIR. 'includes/partials/footer.php');?>
