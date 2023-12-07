c8e04e4cbfba6bc82548a6680eae071eb11b2ecb
lucien-wrq <luluwerq@gmail.com>
2023-12-07 21:04:36 +0100

    Update logo and remove unused image file (generated by blackbox.ai )

---
+++ widgets/la_terre_cest_quoi.php
@@ -0,0 +1,4 @@
+<div class="widget">
+    <h2> La Terre c’est quoi ?</h2>
+    <p>La Terre est entourée d’une mince enveloppe gazeuse : c’est l'<strong> atmosphère !</strong> composé de principalement <strong> d’azote (78%)</strong> et <strong> d'oxygène (21%) </strong>. La surface de la terre est recouverte à <strong> 70% d’océans </strong>, les <strong> 30% </strong> restants de terre émergées étant très inégalement réparties. </p>
+</div>
