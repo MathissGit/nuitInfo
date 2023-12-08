e2fb96f2b621cb5669d55ac1a132039ba4248302
lucien-wrq <luluwerq@gmail.com>
2023-12-08 04:54:38 +0100

    Update Tetris game: - Change language to French. - Double the size of the game board and all related dimensions. (generated by blackbox.ai )

--- errors/tetris.php
+++ errors/tetris.php
@@ -8,12 +8,12 @@
     <div id="tetris">
       <div id="info">
         <div id="next_shape"></div>
-        <p id="level">Level: <span></span></p>
-        <p id="lines">Lines: <span></span></p>
-        <p id="score">Score: <span></span></p>
-        <p id="time">Time: <span></span></p>
-        <button id="start">Start</button>
-        <p class="red">Press the Esc button to pause<span></span></p>
+        <p id="level">Niveau : <span></span></p>
+        <p id="lines">Lignes : <span></span></p>
+        <p id="score">Score : <span></span></p>
+        <p id="time">Temps : <span></span></p>
+        <button id="start">Début</button>
+        <p class="red">échap pour mettre pause<span></span></p>
       </div>
       <div id="canvas"></div>
     </div>
