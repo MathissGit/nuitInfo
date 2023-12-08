6995f2fed8888afec6445caf253c50ac282b2b47
DCodeProg <danael.legrand@outlook.fr>
2023-12-08 04:15:27 +0100

    !!GitHub_Desktop<dev>

---
+++ accueil.php
@@ -0,0 +1,16 @@
+<?php 
+define('ROOT_DIR', '');
+$PAGENAME = "Accueil";
+?>
+
+<?php require_once(ROOT_DIR. 'includes/partials/header.php');?>
+
+<section class="section_play_now">
+    <h1>Testez vos connaissances sur le changement climatique !</h1>
+    <p>Faites vous partie des 47% d'utilisateur à croire à au moins deux idées reçues sur le changement climatique ?</p>
+    <a href="">Jouer maintenant</a>
+</section>
+
+
+
+<?php require_once(ROOT_DIR. 'includes/partials/footer.php');?>
---
+++ includes/partials/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo ROOT_DIR.'assets/css/style.css';?>">
+    <title>Fact Clima</title>
+</head>
+<body>
+    <header>
+        <div class="header_logo">
+            <a href="">
+                <img src="<?php echo ROOT_DIR.'assets/img/logo.png';?>" alt="logo">
+                <h1>Fact Clima</h1>
+            </a>
+        </div>
+        <div class="header_nav">
+            <nav>
+                <a href="">Accueil</a>
+                <a href="">Articles</a>
+                <a href="">Jeu</a>
+            </nav>
+        </div>
+    </header>
